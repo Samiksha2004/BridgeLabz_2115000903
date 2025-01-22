c07937ad8b082419158e318dd3ceaecc80abd92b
Samiksha2004 <94538487+Samiksha2004@users.noreply.github.com>
2025-01-22 12:18:01 +0530

    AgeHarry.java file is added to feature-22/01/2025

---
+++ AgeHarry.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class AgeHarry{
+	public static void main(String args[]){
+		int CUR_YEAR = 2024;
+		int BIRTH_YEAR = 2000;
+		
+		int age = CUR_YEAR - BIRTH_YEAR;
+		System.out.println("Harry's age in 2024 is "+ age);	
+}}

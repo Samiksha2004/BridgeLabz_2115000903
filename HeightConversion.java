6dcdbcd8f01ed6d6f44d9b8736e9f190a195dfc7
Samiksha2004 <94538487+Samiksha2004@users.noreply.github.com>
2025-01-22 16:36:55 +0530

    Height conversion code is added

---
+++ HeightConversion.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HeightConversion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your height in centimeters: ");
+        double heightInCm = input.nextDouble();
+
+        double heightInInches = heightInCm / 2.54;
+
+        int feet = (int) heightInInches / 12;
+        int inches = (int) heightInInches % 12; 
+
+        System.out.println("Your height in cm is " + heightInCm + " while in feet is " + feet + " and inches is " + inches);
+
+    }
+}
+

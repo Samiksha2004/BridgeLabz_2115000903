46b8e1b7e71df91d1debb50c5f1d232371ca2f33
Samiksha2004 <94538487+Samiksha2004@users.noreply.github.com>
2025-01-22 15:08:46 +0530

    for average marks code is added

---
+++ StudentMarks.java
@@ -0,0 +1,13 @@
+public class StudentMarks{
+	public static void main(String args[]){
+		int MATHS_MARKS = 94;
+		int PHYSICS_MARKS = 95;
+		int CHEM_MARKS = 96;
+		
+		double avgMarks = (MATHS_MARKS + PHYSICS_MARKS + CHEM_MARKS)/3.0 ;
+
+		System.out.println("Sam’s average mark in PCM is "+avgMarks);
+		
+	}	
+}
+

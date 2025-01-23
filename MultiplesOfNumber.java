eab66764639cd26ac02e4048ef33887ef82b6b4a
Samiksha2004 <94538487+Samiksha2004@users.noreply.github.com>
2025-01-23 14:41:33 +0530

    MultiplesOfNumbers code is added

---
+++ MultiplesOfNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MultiplesOfNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive number greater than zero.");
+        } else {
+            System.out.println("The multiples of " + number + " below 100 are:");
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+
+       
+    }
+}
+
